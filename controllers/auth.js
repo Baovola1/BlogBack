b9f488b7a16a2a69b08365db3966370452a512d7
Baovola <baovolarav@gmail.com>
2024-03-18 18:40:06 +0100

    create signin api route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 import User from '../models/user.js';
 import bcryptjs from "bcryptjs";
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next)=>{
     const {username, email, password} = req.body;
@@ -23,3 +24,29 @@ export const signup = async (req, res, next)=>{
        next(error);
     }  
 };
+
+export const signin = async(req, res, next)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    try {
+        const userValid = await User.findOne({email});
+        if(!userValid){
+            return next (errorHandler(404, 'User not found'));
+        }
+        const passwordValid = bcryptjs.compareSync(password, userValid.password)
+        if(!passwordValid){
+            next(errorHandler(400, 'Your password is not valid'));
+        }
+        const token = jwt.sign({id:userValid._id},process.env.JWT_SECRET);
+        const{password: pass, ...rest} = userValid._doc;
+
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true}).json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
--- index.js
+++ index.js
@@ -40,3 +40,4 @@ app.listen(process.env.PORT, () => {
       message
     })
   })
+  
